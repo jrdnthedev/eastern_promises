b390350fccd099001b8dbd88fa5e513351d7c23d
andre mcmorris <ajmcmorris@gmail.com>
2025-01-28 16:14:50 -0500

    removed boilerplate code added basic app structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from '../core/components/sidebar/sidebar.component';
+import { FooterComponent } from '../core/components/footer/footer.component';
+import { HeaderComponent } from '../core/components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarComponent, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'dashboard';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from '../pages/dashboard/dashboard.component';
+import { CustomersComponent } from '../pages/customers/customers.component';
+import { InvoicesComponent } from '../pages/invoices/invoices.component';
+import { RevenueComponent } from '../pages/revenue/revenue.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: 'customers',
+    component: CustomersComponent,
+  },
+  {
+    path: 'invoices',
+    component: InvoicesComponent,
+  },
+  {
+    path: ' revenue',
+    component: RevenueComponent,
+  },
+];
