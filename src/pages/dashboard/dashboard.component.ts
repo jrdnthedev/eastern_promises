2c39f2f2e5ba050b386782a4feaf40c2b92f2199
andre mcmorris <ajmcmorris@gmail.com>
2025-01-28 18:41:03 -0500

    modified page to handle more data

--- src/pages/dashboard/dashboard.component.ts
+++ src/pages/dashboard/dashboard.component.ts
@@ -1,22 +1,36 @@
 import { Component, inject } from '@angular/core';
 import { CustomerServiceService } from '../../services/customer-service/customer-service.service';
-import { Customer } from '../../types/types';
+import { Customer, Invoice, Revenue } from '../../types/types';
+import { CardListComponent } from '../../components/card-list/card-list.component';
+import { InvoiceService } from '../../services/invoice-service/invoice.service';
+import { RevenueService } from '../../services/revenue-service/revenue.service';
+import { forkJoin } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CardListComponent, CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
   private customerService = inject(CustomerServiceService);
+  private invoiceService = inject(InvoiceService);
+  private revenueService = inject(RevenueService);
   customers: Customer[] = [];
+  invoices: Invoice[] = [];
+  revenue: Revenue[] = [];
 
   ngOnInit() {
-    this.customerService.getCustomers().subscribe((data: Customer[]) => {
-      console.log(data);
-      this.customers = data;
+    forkJoin([
+      this.customerService.getCustomers(),
+      this.invoiceService.getInvoices(),
+      this.revenueService.getRevenue(),
+    ]).subscribe(([customers, invoices, revenue]) => {
+      this.customers = customers;
+      this.invoices = invoices;
+      this.revenue = revenue;
     });
   }
 }
