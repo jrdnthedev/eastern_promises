3d8192646ca586b1bc5ee16b60503515c2751530
andre mcmorris <ajmcmorris@gmail.com>
2025-01-29 16:39:36 -0500

    fixed update customer bug

--- src/components/edit-modal/edit-modal.component.ts
+++ src/components/edit-modal/edit-modal.component.ts
@@ -1,26 +1,45 @@
 import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { Customer } from '../../types/types';
-import { FormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { CustomerServiceService } from '../../services/customer-service/customer-service.service';
 
 @Component({
   selector: 'app-edit-modal',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './edit-modal.component.html',
   styleUrl: './edit-modal.component.scss',
 })
 export class EditModalComponent {
   @Input() customer!: Customer;
+  updatedCustomer!: FormGroup;
   private customerService = inject(CustomerServiceService);
   @Output() destroy = new EventEmitter<void>();
+
+  ngOnInit() {
+    this.updatedCustomer = new FormGroup({
+      name: new FormControl(this.customer.name, [Validators.required]),
+      email: new FormControl(this.customer.email, [Validators.required]),
+    });
+  }
 
   closeModal() {
     console.log('Close modal');
     this.destroy.emit();
   }
 
-  updateCustomer(customer: Customer) {
+  updateCustomer() {
+    const customer: Customer = {
+      ...this.customer,
+      name: this.updatedCustomer.value.name,
+      email: this.updatedCustomer.value.email,
+    };
     this.customerService.editCustomer(customer).subscribe();
     this.closeModal();
   }
