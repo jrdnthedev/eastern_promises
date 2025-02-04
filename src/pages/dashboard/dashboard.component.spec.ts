fae08b1c340e85742aeaf648ed7fe01246e74b5c
andre mcmorris <ajmcmorris@gmail.com>
2025-02-03 19:38:20 -0500

    added dashboard tests

--- src/pages/dashboard/dashboard.component.spec.ts
+++ src/pages/dashboard/dashboard.component.spec.ts
@@ -1,17 +1,42 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardComponent } from './dashboard.component';
+import { CustomerServiceService } from '../../services/customer-service/customer-service.service';
+import { InvoiceService } from '../../services/invoice-service/invoice.service';
+import { of } from 'rxjs';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
+  let customerService: jasmine.SpyObj<CustomerServiceService>;
+  let invoiceService: jasmine.SpyObj<InvoiceService>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [DashboardComponent]
-    })
-    .compileComponents();
+    const customerServiceSpy = jasmine.createSpyObj(
+      'CustomerServiceService',
+      ['getCustomers'],
+      { latestCustomers$: of([]) }
+    );
+    const invoiceServiceSpy = jasmine.createSpyObj(
+      'InvoiceService',
+      ['getInvoices'],
+      { latestInvoices$: of([]) }
+    );
 
+    await TestBed.configureTestingModule({
+      imports: [DashboardComponent],
+      providers: [
+        { provide: CustomerServiceService, useValue: customerServiceSpy },
+        { provide: InvoiceService, useValue: invoiceServiceSpy },
+      ],
+    }).compileComponents();
+
+    customerService = TestBed.inject(
+      CustomerServiceService
+    ) as jasmine.SpyObj<CustomerServiceService>;
+    invoiceService = TestBed.inject(
+      InvoiceService
+    ) as jasmine.SpyObj<InvoiceService>;
     fixture = TestBed.createComponent(DashboardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +44,27 @@ describe('DashboardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call getCustomers on customerService during ngOnInit', () => {
+    expect(customerService.getCustomers).toHaveBeenCalled();
+  });
+
+  it('should call getInvoices on invoiceService during ngOnInit', () => {
+    expect(invoiceService.getInvoices).toHaveBeenCalled();
+  });
+
+  it('should initialize customers$ observable from customerService', (done) => {
+    component.customers$.subscribe((customers) => {
+      expect(customers).toEqual([]);
+      done();
+    });
+  });
+
+  it('should initialize invoices$ observable from invoiceService', (done) => {
+    component.invoices$.subscribe((invoices) => {
+      expect(invoices).toEqual([]);
+      done();
+    });
   });
 });
