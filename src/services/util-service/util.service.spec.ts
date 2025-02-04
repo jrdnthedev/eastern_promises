313e65e8fc57edc02915b8980a4cc14e50c506e8
andre mcmorris <ajmcmorris@gmail.com>
2025-02-03 19:58:07 -0500

    added util test

--- src/services/util-service/util.service.spec.ts
+++ src/services/util-service/util.service.spec.ts
@@ -6,11 +6,28 @@ describe('UtilService', () => {
   let service: UtilService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [UtilService],
+    });
     service = TestBed.inject(UtilService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should generate a valid UUID', () => {
+    const uuid = service.generateUUID();
+
+    // UUID pattern: xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx
+    const uuidRegex =
+      /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+    expect(uuid).toMatch(uuidRegex);
+  });
+
+  it('should generate unique UUIDs', () => {
+    const uuid1 = service.generateUUID();
+    const uuid2 = service.generateUUID();
+    expect(uuid1).not.toEqual(uuid2);
   });
 });
