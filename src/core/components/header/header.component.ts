f460fddcff9b2ff49d198dda87f520c330f631ed
andre mcmorris <ajmcmorris@gmail.com>
2025-03-19 08:31:58 -0400

    added dynamic route heading to header component

--- src/core/components/header/header.component.ts
+++ src/core/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  private router = inject(Router);
+  currentRoute = signal(this.router.url);
+
+  constructor() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        const route = event.urlAfterRedirects.replace('/', '');
+        this.currentRoute.set(route);
+      }
+    });
+  }
+}
