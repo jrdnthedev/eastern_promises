96fe5627f787f210b6a0ae6b559b165b39565b2f
andre mcmorris <ajmcmorris@gmail.com>
2025-03-23 02:43:50 -0400

    app enhancements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from '../core/components/sidebar/sidebar.component';
-import { FooterComponent } from '../core/components/footer/footer.component';
 import { HeaderComponent } from '../core/components/header/header.component';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [SidebarComponent, FooterComponent, HeaderComponent, RouterModule],
+  imports: [SidebarComponent, HeaderComponent, RouterModule],
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
