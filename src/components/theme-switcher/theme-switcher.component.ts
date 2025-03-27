9e66caa8d5a1cb6cd85a4dfc292bd5cbfdb4b2df
andre mcmorris <ajmcmorris@gmail.com>
2025-03-27 07:59:17 -0400

    added dark mode

--- src/components/theme-switcher/theme-switcher.component.ts
+++ src/components/theme-switcher/theme-switcher.component.ts
@@ -8,14 +8,12 @@ import { ThemeService } from '../../services/theme-service/theme.service';
   styleUrl: './theme-switcher.component.scss',
 })
 export class ThemeSwitcherComponent {
-  themes = ['light-theme', 'dark-theme'];
+  themes = ['light', 'dark'];
   themeService = inject(ThemeService);
   isThemeDark = signal(false);
 
   toggleTheme() {
     this.isThemeDark.update((isDark) => !isDark);
-    this.themeService.setTheme(
-      this.isThemeDark() ? 'dark-theme' : 'light-theme'
-    );
+    this.themeService.setTheme(this.isThemeDark() ? 'dark' : 'light');
   }
 }
