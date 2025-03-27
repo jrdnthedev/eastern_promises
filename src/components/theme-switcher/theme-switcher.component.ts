f8bd62c6a5c3180625b3171cc7c443a2acbc6d7f
andre mcmorris <ajmcmorris@gmail.com>
2025-03-27 12:06:35 -0400

    fixed background height issue

--- src/components/theme-switcher/theme-switcher.component.ts
+++ src/components/theme-switcher/theme-switcher.component.ts
@@ -8,12 +8,12 @@ import { ThemeService } from '../../services/theme-service/theme.service';
   styleUrl: './theme-switcher.component.scss',
 })
 export class ThemeSwitcherComponent {
-  themes = ['light', 'dark'];
+  themes = ['', 'dark'];
   themeService = inject(ThemeService);
   isThemeDark = signal(false);
 
   toggleTheme() {
     this.isThemeDark.update((isDark) => !isDark);
-    this.themeService.setTheme(this.isThemeDark() ? 'dark' : 'light');
+    this.themeService.setTheme(this.isThemeDark() ? 'dark' : '');
   }
 }
